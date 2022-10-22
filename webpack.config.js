4f18f3e83e380196634f0c6d048ac5b083b6fca3
Fadlian17 <alfansyahfadlian@gmail.com>
2022-10-22 15:20:53 +0700

    create header component 221022

--- webpack.config.js
+++ webpack.config.js
@@ -89,6 +89,14 @@ module.exports = {
           globOptions: {
             ignore: ["*.DS_Store", "Thumbs.db"],
           },
+        },        
+        {
+          from: path.resolve(environment.paths.source, "images", "design"),
+          to: path.resolve(environment.paths.output, "images", "design"),
+          toType: "dir",
+          globOptions: {
+            ignore: ["*.DS_Store", "Thumbs.db"],
+          },
         },
         {
           from: path.resolve(environment.paths.source, "css"),
